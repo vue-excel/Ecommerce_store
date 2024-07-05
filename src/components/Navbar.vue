b2f7dcc48a348f237b5478fd0303fef73668ee17
vue-excel <xspace781@gmail.com>
2024-07-05 19:06:32 +0530

    worked on navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nav-container">
+    <router-link to="/">
+      <div class="logo">Shazz</div>
+    </router-link>
+    <a-input-search
+      placeholder="input search text"
+      enter-button
+      style="width: 250px"
+      @search="onSearch"
+      v-model="searchTerm"
+    />
+    <router-link to="/cart">
+      <a-badge :count="allProductLength">
+        <a href="#" class="head-example" />
+        <a-icon
+          type="shopping-cart"
+          :style="{ fontSize: '20px', marginTop: '3px' }"
+        />
+      </a-badge>
+    </router-link>
+  </div>
+</template>
