243bec8246cae120c36f5d562e843687045676e5
vue-excel <xspace781@gmail.com>
2024-07-08 23:35:37 +0530

    added style

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -44,3 +44,117 @@
     </a-carousel>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style scoped>
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 500px;
+  overflow: hidden;
+}
+.ant-carousel >>> .slick-slide h3 {
+  color: #000;
+}
+.ant-carousel >>> .slick-dots li button {
+  background: #000;
+}
+.ant-carousel >>> .slick-dots li.slick-active button {
+  background: #1890ff !important;
+}
+.carousel-wrapper {
+  position: relative;
+}
+.carousel-wrapper > div {
+  position: absolute;
+  top: 160px;
+  left: 120px;
+}
+
+.ant-carousel >>> .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: rgb(2, 2, 2);
+  /* background-color: rgba(31, 45, 61, 0.11); */
+  opacity: 0.3;
+}
+.ant-carousel >>> .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel >>> .custom-slick-arrow:hover {
+  color: rgb(2, 2, 2);
+  opacity: 0.7;
+}
+
+@media (max-width: 1000px) {
+  .ant-carousel >>> .slick-slide {
+    text-align: center;
+    height: 350px;
+    overflow: hidden;
+  }
+  .carousel-wrapper > div {
+    position: absolute;
+    top: 100px;
+    left: 120px;
+  }
+  .smallfont {
+    font-size: 12px;
+  }
+}
+@media (max-width: 700px) {
+  .ant-carousel >>> .slick-slide {
+    text-align: center;
+    height: 250px;
+    overflow: hidden;
+  }
+  .carousel-wrapper > div {
+    position: absolute;
+    top: 20px;
+    left: 120px;
+  }
+  .smallfont {
+    font-size: 10px;
+  }
+}
+@media (max-width: 550px) {
+  .smallfont {
+    font-size: 7px;
+  }
+}
+@media (max-width: 450px) {
+  .ant-carousel >>> .slick-slide {
+    text-align: center;
+    height: 250px;
+    overflow: hidden;
+  }
+  .carousel-wrapper > div {
+    position: absolute;
+    top: 40px;
+    left: 80px;
+  }
+  .smallfont {
+    font-size: 7px;
+  }
+}
+@media (max-width: 360px) {
+  .ant-carousel >>> .slick-slide {
+    text-align: center;
+    height: 150px;
+    overflow: hidden;
+  }
+  .carousel-wrapper > div {
+    position: absolute;
+    top: 10px;
+    left: 60px;
+  }
+  .smallfont {
+    font-size: 5px;
+  }
+}
+</style>
+
